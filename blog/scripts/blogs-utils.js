887873fda4d88ca855edf74ddfa828329c2e5987
Vashirawit <vakkharasaksiri@hotmail.com>
2024-01-13 13:34:10 +0700

    create function and change content into variable

--- blog/scripts/blogs-utils.js
+++ blog/scripts/blogs-utils.js
@@ -2,23 +2,38 @@ console.log("Hello Skooldio")
 
 const blogElement =document.getElementById('blog-container')
 
-blogElement.innerHTML = `
+function createBlogHTML(blog) {
+    blogElement.innerHTML = `
     <div class="flex flex-col md:flex-row gap-6 w-full">
         <img
-          src="https://fastly.picsum.photos/id/485/300/200.jpg?hmac=N6yRTUogA1s8HlQMCO9BtxlXa8mg7AOQs6JKw0jq6Fw"
+          src="${blog.imageUrl}"
           alt="feature image 1"
           class="w-full md:w-auto"
         />
         <div class="flex flex-col gap-4 bg-wd-darkgrey p-6 grow">
           <h3 class="text-2xl font-semibold">
-            Forward Integration Executive
+            ${blog.title}
           </h3>
           <p class="text-xl font-light">
-            Nobis quo est corporis totam dolores. Rerum quam autem debitis
-            dolores sunt et quis occaecati. Nam dolorem dolores.
+            ${blog.description}
           </p>
-          <p>At 4/1/2024</p>
+          <p>At ${blog.publishedDate}</p>
           <a href="blogs/test.html">Read more</a>
         </div>
       </div>
-`
+    `
+}
+
+
+// Create a variable of content
+const blog = {
+    "title": "Forward Integration Executive",
+    "description": "Nobis quo est corporis totam dolores. Rerum quam autem debitis dolores sunt et quis occaecati. Nam dolorem dolores. ",
+    "publishedDate": "4/1/2024",
+    "imageUrl": "https://fastly.picsum.photos/id/48/300/200.jpg?hmac=5ZylZrVu2ikHu7ODDfsD-Po4--cyrccMtvkzQpf3arE"
+ }
+
+ createBlogHTML(blog)
+
+
+
